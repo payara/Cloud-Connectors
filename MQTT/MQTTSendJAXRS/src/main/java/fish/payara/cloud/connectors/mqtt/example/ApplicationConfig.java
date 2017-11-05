a65ffaf57e0ad9c2ae58b9da218c0b50d44227d5
Steve Millidge <smillidge@users.noreply.github.com>
2017-11-05 21:15:13 +0000

    Split out the MQTT Examples into a sender and a receiver for Java One (#32)
    * Split out the MQTT Examples into a sender and a receiver for Java One
    
    * Added JAX-RS sender so can demonstrate at Java One
    
    * Add Send JAX-RS to the pom

---
+++ MQTT/MQTTSendJAXRS/src/main/java/fish/payara/cloud/connectors/mqtt/example/ApplicationConfig.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fish.payara.cloud.connectors.mqtt.example;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author steve
+ */
+@javax.ws.rs.ApplicationPath("demo")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(fish.payara.cloud.connectors.mqtt.example.EventsResource.class);
+    }
+    
+}
